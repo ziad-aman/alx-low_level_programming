476770a955a134a62deea6c3acb336bfa7c964e7
ziad-aman <ziadhassan.zh66@gmail.com>
2023-02-17 16:24:08 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  *  main - A progam that prints a line to the standard error
  *  Return 1 (Success)
  */
-int main (void)
+int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
