58a4f070cc5f81121744438eb3b2a172784386a1
ziad-aman <ziadhassan.zh66@gmail.com>
2023-02-17 18:07:44 -0800

    possible 2 compination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- * Description: prints numbers
- * Return: Always 0 (success)
+ *main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
+int i, j;
 
-	for (i = '0'; i <= '9' ; i++)
-	{
-		putchar(i);
-
-		if (i != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
-	return (0);
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
 }
+}
+}
+}
+putchar('\n');
+return (0);
+}}
