ad318434d0fdb9a9a8617ef85c6561c6a80f6d2c
ziad-aman <ziadhassan.zh66@gmail.com>
2023-03-13 11:52:55 -0700

    rgs to str

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,17 +19,13 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
+		len++;
 		len++;
 	}
-
 	str = malloc(sizeof(char) * (len + 1));
-
 	if (str == NULL)
 		return (NULL);
-
 	k = 0;
-
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
